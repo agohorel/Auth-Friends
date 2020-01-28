f6b487823f521cb522367f589c6272d8fe045807
agohorel <gohorelalex@gmail.com>
2020-01-28 16:14:50 -0500

    add PrivateRoute component

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import PrivateRoute from "./components/PrivateRoute";
 import LoginForm from "./components/LoginForm";
+import FriendsList from "./components/FriendsList";
 
 function App() {
   return (
     <div>
-      <LoginForm></LoginForm>
+      <Router>
+        <Route path="/login" component={LoginForm} />
+        <PrivateRoute path="/friends" component={FriendsList} />
+      </Router>
     </div>
   );
 }
---
+++ auth-friends/src/components/FriendsList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FriendsList = () => {
+  return <div>secret friends go here</div>;
+};
+
+export default FriendsList;
